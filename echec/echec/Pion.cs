49fec84bf0c5ba8f6fc0bef9baa022b7f7b79874
Valentin889 <valentin.schneeberger@rpn.ch>
2017-03-10 09:48:47 +0100

    ajout d'une méthode de deplacement pour les pions

--- echec/echec/Pion.cs
+++ echec/echec/Pion.cs
@@ -8,14 +8,29 @@ namespace echec
 {
     public class Pion : Piece
     {
+        bool PremierDeplacement;
         public Pion(string couleur)
             : base(couleur)
         {
-
+            PremierDeplacement = true;
         }
         public override List<string> DeplacementPossible(Jeu jeu)
         {
-            throw new NotImplementedException();
+            List<String> renvoie = new List<string>();
+            Renvoie = renvoie;
+
+            if (PositionY + 1 > jeu.TabPiece.Length)
+            {
+                Deplacement(jeu, PositionY + 1, PositionX);
+                if (PremierDeplacement)
+                {
+                    if (PositionY + 2 > jeu.TabPiece.Length)
+                    {
+                        Deplacement(jeu, PositionY + 2, PositionX);
+                    }
+                }
+            }
+            return Renvoie;
         }
     }
 }
